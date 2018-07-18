7b67a515de80ab42c152c336ad1507dcb0471315
alexfevery <alexfevery@gmail.com>
2018-07-18 11:02:31 -0700

    Adding pages

---
+++ Portfolio/dictionary/index.php
@@ -0,0 +1,46 @@
+<?php
+include "../../common1.txt";
+?>	
+
+<?php
+include "../../common2.txt";
+?>	
+				
+		<div align="Center">
+		<p>
+		<h1>Shinjishun Chinese-English Dictionary</h1>
+		</p>
+	</div>
+	
+	
+	<div class="container">
+			<div class="jumbotron text-center">
+				<div align="left" style="font-size:24px">
+					<br>
+					<h2></h2>
+					<p>
+					
+					<br>
+					<li>Ambitious Chinese-English dictionary project with over 550,000 entries</li>
+					<li>Over 40,000 example sentences</li>
+					<li>Home page automatically updated with Chinese news articles with tooltip mouseover popup definitions of words</li>
+					<li>Includes both simplified and traditional Chinese as well as Mainland China and Taiwan pronunciation</li>
+					<li>Dictionary site is connected to an HTTP server I designed which puts queries through multiple algorithms to provide results</li>
+					<li>Multithreaded server puts queries through all 550,000 entries and 40,000 sentences using algorithms for multiple search types (english, chinese, pinyin) in less than 100ms.</li>
+					
+					<br><br>
+					<img src = "howto//sl1.jpg" alt = "studylock"/>
+					<br><br><br>
+					<img src = "howto//sl2.jpg" alt = "studylock"/>
+					<br><br><br>
+					<img src = "howto//sl3.jpg" alt = "studylock"/>
+					</p>
+					
+				</div>
+				<br>
+			</div>
+		</div>
+ 
+<?php
+include "../../common3.txt";
+?>	                
